07f0ed32448ab5e472a96cadf5d6091516df4d9a
Mário Luis Braz Junior <jrmario99@gmail.com>
2020-12-07 17:55:23 -0300

    07/12 - Consumir API e Gravar em BD
    Funncionalidade de consumir API e gravar em Banco OK

--- AtividadeAula2/AtividadeAula2/Controllers/CasosController.cs
+++ AtividadeAula2/AtividadeAula2/Controllers/CasosController.cs
@@ -3,14 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AtividadeAula2.Models;
+using AtividadeAula2.DAL;
 
 namespace AtividadeAula2.Controllers
 {
     public class CasosController : Controller
     {
+
+        private readonly Context _context;
+        CasosDAL casosDAL = new CasosDAL();
+
+
+        public CasosController(Context context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult Index()
+        {
+            List<Casos> casos = new List<Casos>();
+
+            casos = casosDAL.listarCasos(_context);
+
+            return View(casos);
+        }
+
+        public IActionResult Cadastrar()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(Casos caso)
+        {
+
+
+            if (casosDAL.addCasos(caso, _context))
+            {
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+
+                return RedirectToAction("Cadastrar");
+            }
+
+
+        }
+
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
+
+
+
     }
 }
---
+++ AtividadeAula2/AtividadeAula2/DAL/CasosDAL.cs
@@ -0,0 +1,48 @@
+﻿using AtividadeAula2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtividadeAula2.DAL
+{
+    public class CasosDAL
+    {
+
+        public Boolean addCasos(Casos p, Context _context)
+        {
+            _context.Add(p);
+
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Casos> listarCasos(Context _context)
+        {
+            List<Casos> casos = new List<Casos>();
+
+            casos = _context.Casos.ToList();
+
+            if (casos.Count == 0)
+            {
+
+                return null;
+            }
+
+
+            return casos;
+
+        }
+
+
+
+    }
+}
